fd5e201d60f0cddec20389bbdb35f0d51f6f806d
fortwoone <denauxarthur@gmail.com>
2026-01-28 11:50:15 +0100

    fix #3.3: Functional REST Controller

--- src/main/java/ourbusinessproject/ourbusinessproject/Bootstrap.java
+++ src/main/java/ourbusinessproject/ourbusinessproject/Bootstrap.java
@@ -1,7 +1,6 @@
 package ourbusinessproject.ourbusinessproject;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
---
+++ src/main/java/ourbusinessproject/ourbusinessproject/ProjectController.java
@@ -0,0 +1,33 @@
+package ourbusinessproject.ourbusinessproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@RestController
+public class ProjectController {
+    public record ReturnedProject(String title, String description, String enterprise){
+        public ReturnedProject(Project p){
+            this(p.getTitle(), p.getDescription(), p.getEnterprise().getName());
+        }
+    }
+
+    private EnterpriseProjectService enterpriseProjectService;
+
+    public ProjectController(EnterpriseProjectService enterpriseProjectService){
+        this.enterpriseProjectService = enterpriseProjectService;
+    }
+
+    @GetMapping("/api/projects")
+    public @ResponseBody List<ReturnedProject> findAllProjectsWithEnterprises(){
+        List<Project> projects = enterpriseProjectService.findAllProjects();
+        List<ReturnedProject> returned = new ArrayList<>();
+
+        projects.forEach((Project p) -> returned.add(new ReturnedProject(p)));
+
+        return returned;
+    }
+}
